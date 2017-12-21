39456708288b1c3c9a8ef74f06dc1321f7a2a2bb
Felix Bühler <felixcool98@t-online.de>
2017-12-21 01:53:09 +0100

    improved tooltips

--- default/generator.py
+++ default/generator.py
@@ -6,13 +6,13 @@ Created on 26.11.2017
 import numpy as np
 import random
 import body
-import system
+from system import System
 import math
 
 class default_gen:
     @staticmethod
     def generate(bodyamount, minmass, maxmass, minrad, maxrad, scale, centermass, centerrad):
-        sys = system.System()
+        sys = System()
         
         sys.add_centre(body.Body(centermass, centerrad, scale, maxrad,r=1.0, g=1.0,b=0))
         
@@ -23,7 +23,6 @@ class default_gen:
             volume = 4/3*math.pi*newrad**3
             newmass = volume*density
             
-            
             newdir = np.array((random.random()*2-1, random.random()*2-1))
             
             angle = 2*random.random()*math.pi
@@ -33,9 +32,11 @@ class default_gen:
             y = radius * math.sin(angle)
             
             newpos = np.array((x*scale, y*scale, (random.random()*2-1)*scale/10), dtype=np.float64)
+            
             r = random.random()
             g = random.random()
             b = random.random()
-            sys.add_planet(body.Body(newmass, newrad, maxrad, newdir, newpos,r=r, g=g,b=b), newpos)
+            
+            sys.add_planet(body.Body(newmass, newrad, maxrad, newdir, newpos, r=r, g=g, b=b), newpos)
         
         return sys
